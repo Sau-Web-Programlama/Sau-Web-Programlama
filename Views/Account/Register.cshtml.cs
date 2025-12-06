0bfd12de3f79809c57ad821d597150caee76cb77
Tunahan avsar <114258916+Ahazzzz@users.noreply.github.com>
2025-12-06 23:37:41 +0300

    6.12.2025

---
+++ Views/Account/Register.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Spor.Salonu.Views.Account
+{
+    public class RegisterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Views/Trainers/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Spor.Salonu.Views.Trainers
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
