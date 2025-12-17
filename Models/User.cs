4a0076f8c316795649d74fd5f0a1fb987ce744f7
Tunahan avsar <114258916+Ahazzzz@users.noreply.github.com>
2025-12-17 23:32:02 +0300

    17.12.2025

--- Models/User.cs
+++ Models/User.cs
@@ -8,5 +8,8 @@
         public string Email { get; set; }
         public string Password { get; set; }
         public string Role { get; set; } = "Member";
+
+        // EKSİK OLAN BU SATIRI EKLE:
+        public string Phone { get; set; }
     }
 }
