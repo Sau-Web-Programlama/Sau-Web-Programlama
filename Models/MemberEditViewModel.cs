23ed83835c0c7e5adc0e014f8723cda874d6733e
o1an1l <a.gezertasar04@gmail.com>
2025-12-18 20:50:32 +0300

    18.12.2025

---
+++ Models/MemberEditViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SporSalonu2.Models
+{
+    public class MemberEditViewModel
+    {
+        // HATANIN SEBEBİ BURASIYDI (string yerine int yaptık)
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Ad alanı zorunludur.")]
+        [Display(Name = "Ad")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Soyad alanı zorunludur.")]
+        [Display(Name = "Soyad")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email alanı zorunludur.")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Display(Name = "Telefon")]
+        public string Phone { get; set; }
+    }
+}
